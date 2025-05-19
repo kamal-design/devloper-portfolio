722c7b39d9a4647c635fe977226af9e3b0078480
kamaledvoy <smkamal.sm@gmail.com>
2025-05-19 10:59:44 +0530

    codepen demo link added

---
+++ src/components/gsap/ HighlightingOnScroll.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HighlightingOnScroll = () => {
+  // https://codepen.io/akapowl/pen/YzMoYLd
+  // https://codepen.io/akapowl/pen/bGyMGxr
+  // https://codepen.io/akapowl/pen/rNeqQzW/4a8ca13036ba9f2f80919f671da1fed0
+  // https://codepen.io/akapowl/pen/jOqYRGW/21b32f0bd91e3795a831604fe95f27f4
+  //codepen.io/akapowl/pen/qBzBXJJ
+
+  https: return <div> HighlightingOnScroll</div>;
+};
+
+export default HighlightingOnScroll;
